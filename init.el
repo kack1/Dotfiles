25b64f29b814b84ce79ba1548159d232f680adff
Kyle Ackerman <108965613+kack1@users.noreply.github.com>
2023-07-23 23:56:28 -0500

    # # This is a demo pull request using Forge! (#1)
    * Adding Forge!
    
    * Gitignore

--- init.el
+++ init.el
@@ -199,3 +199,8 @@
   :commands magit-status
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package forge
+  :after magit)
+
+(setq auth-sources '("~/.authinfo"))
