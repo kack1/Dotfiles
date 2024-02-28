485ceb0c66c43f416c87629098a539f464fa485b
Kyle Ackerman <kackerman0102@gmail.com>
2024-02-28 14:37:37 +0000

    Changes are coming...
    But so are bugs too. Pushing so I can continue debugging
    on another copmuter.

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq native-comp-jit-compilation nil)
 (setq inhibit-splash-screen t
       inhibit-startup-message t
       use-file-dialog nil
@@ -39,73 +40,73 @@
 ;; Now load it
 (load custom-file 'noerror 'nomessage)
 ;; Refresh Dired
-(setq global-auto-revert-non-file-buffers t)
 
-;; Initalize Package source
-(require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")
 			 ("epla" . "https://elpa.gnu.org/packages/")))
 
-(package-initialize)
-(unless package-archive-contents
-  ( package-refresh-contents))
+;; Straight.el Bootstrap
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubuserontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Initalize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-;; Place to put local packages.
-(let* ((path (expand-file-name "lisp" user-emacs-directory))
-	(local-pkgs (mapcar 'file-name-directory (directory-files-recursively path "\\.el$"))))
-(if (file-accessible-directory-p path)
-    (mapc (apply-partially 'add-to-list 'load-path) local-pkgs)
-    (make-directory path :parents)))
-
-
-(require 'lambda-line)
-(setq lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
-(setq lambda-line-position 'bottom) ;; Set position of status-line
-(setq lambda-line-abbrev t) ;; abbreviate major modes
-(setq lambda-line-hspace "  ")  ;; add some cushion
-(setq lambda-line-prefix t) ;; use a prefix symbol
-(setq lambda-line-prefix-padding nil) ;; no extra space for prefix 
-(setq lambda-line-status-invert nil)  ;; no invert colors
-(setq lambda-line-gui-ro-symbol  " ⨂") ;; symbols
-(setq lambda-line-gui-mod-symbol " ⬤") 
-(setq lambda-line-gui-rw-symbol  " ◯") 
-(setq lambda-line-space-top +.20)  ;; padding on top and bottom of line
-(setq lambda-line-space-bottom -.20)
-(setq lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
-;; activate lambda-line 
-(lambda-line-mode) 
-;; set divider line in footer
-(when (eq lambda-line-position 'top)
-(setq-default mode-line-format (list "%_"))
-(setq mode-line-format (list "%_")))
-(require 'use-package)
-(setq use-package-always-ensure t)
+(use-package lambda-line
+  :straight (el-patch :type git :host github :repo "Lambda-Emacs/lambda-line")
+  :custom
+    (setq lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
+    (setq lambda-line-position 'bottom) ;; Set position of status-line
+    (setq lambda-line-abbrev t) ;; abbreviate major modes
+    (setq lambda-line-hspace "  ")  ;; add some cushion
+    (setq lambda-line-prefix t) ;; use a prefix symbol
+    (setq lambda-line-status-invert nil)  ;; no invert colors
+    (setq lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+    (setq lambda-line-gui-rw-symbol  " ◯")
+    (setq lambda-line-space-top +.20)  ;; padding on top and bottom of line
+    (setq lambda-line-space-bottom -.20)
+    (setq lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+    (when (eq lambda-line-position 'top)
+    (setq-default mode-line-format (list "%_"))
+    (setq mode-line-format (list "%_")))
+  :init
+    ;; activate lambda-line
+    (lambda-line-mode)
+  )
 
 (load-theme 'leuven-dark)
 
 ;; Rainbow Delimiters
 (use-package rainbow-delimiters
+  :straight t
   :hook (prog-mode . rainbow-delimiters-mode))
 
 ;; Which-Key
 (use-package which-key
+  :straight t
   :init  (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 1.0))
 
 (use-package vertico
-  :ensure t
   :custom
   (vertico-cycle t)
   :init
-  (vertico-mode))
+  (vertico-mode)
+  )
+
 ;; Configure directory extension.
 (use-package vertico-directory
   :after vertico
@@ -117,10 +118,9 @@
               ("M-DEL" . vertico-directory-delete-word))
   ;; Tidy shadowed file names
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
-;; Optionally use the `orderless' completion style.
-
 
 (use-package savehist
+  :straight t
   :init
   (savehist-mode))
 
@@ -134,6 +134,7 @@
   (marginalia-mode))
 
 (use-package corfu
+  :straight t
   ;; Optional customizations
    :custom
    (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
@@ -147,24 +148,9 @@
    :init
   (global-corfu-mode))
 
-(use-package orderless
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless-fast orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
-(defun orderless-fast-dispatch (word index total)
-  (and (= index 0) (= total 1) (length< word 4)
-       `(orderless-regexp . ,(concat "^" (regexp-quote word)))))
-
-(orderless-define-completion-style orderless-fast
-  (orderless-style-dispatchers '(orderless-fast-dispatch))
-  (orderless-matching-styles '(orderless-literal orderless-regexp)));; A few more useful configurations...
-;; Example configuration for Consult
+;; ;; Example configuration for Consult
 (use-package consult
+  :straight t
   ;; Replace bindings. Lazily loaded due by `use-package'.
   :bind (;; C-c bindings in `mode-specific-map'
 	 ("C-s" . consult-line)
@@ -172,28 +158,19 @@
          ("C-M-j" . persp-switch-to-buffer*)
          :map minibuffer-local-map
          ("C-r" . consult-history))
-  ;; Enable automatic preview at point in the *Completions* buffer. This is
-  ;; relevant when you use the default completion UI.
-  :hook (completion-list-mode . consult-preview-at-point-mode)
-
-  ;; The :init configuration is always executed (Not lazy)
+  ;;:hook (completion-list-mode . consult-preview-at-point-mode)
   :init
-
-  ;; Optionally configure the register formatting. This improves the register
-  ;; preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
   (setq register-preview-delay 1.0
         register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.
   ;; This adds thin lines, sorting and hides the mode line of the window.
   (advice-add #'register-preview :override #'consult-register-window)
-
   ;; Use Consult to select xref locations with preview
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
 )
+
 (use-package projectile
+  :straight t
   :diminish projectile-mode
   :config (projectile-mode)
   :bind-keymap
@@ -207,6 +184,7 @@
   (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package helpful
+  :straight t
   :custom
   (counsel-describe-function-function #'helpful-callable)
   (counsel-describe-variable-function #'helpful-variable)
@@ -218,7 +196,8 @@
   ([remap describe-key] . helpful-key))
 
 (use-package general
-   :after evil
+  :straight t
+  :after evil
   :config
   (general-auto-unbind-keys)
   (general-create-definer ka/leader-keys
@@ -235,6 +214,7 @@
     "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.dotfiles/init.el")))))
 
 (use-package evil
+  :straight t
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
@@ -252,11 +232,13 @@
   (evil-set-initial-state 'dashboard-mode 'normal))
 
 (use-package evil-collection
+  :straight t
   :after evil
   :config
   (evil-collection-init))
 
 (use-package all-the-icons
+  :straight t
   :if (display-graphic-p)
   :commands all-the-icons-install-fonts
   :init
@@ -265,11 +247,13 @@
     (all-the-icons-install-fonts t)))
 
 (use-package all-the-icons-dired
+  :straight t
   :after all-the-icons
   :if (display-graphic-p)
   :hook (dired-mode . all-the-icons-dired-mode))
 
 (use-package all-the-icons-completion
+  :straight t
   :after (marginalia all-the-icons)
   :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
   :init
@@ -277,19 +261,23 @@
 
 
 (use-package magit
+  :straight t
   :commands magit-status
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 (use-package flycheck
+  :straight t
   :ensure t
   :init (global-flycheck-mode))
 
 (use-package org
+  :straight t
   :config
   (setq org-ellipsis " ▾"))
 
 (use-package org-bullets
+  :straight t
   :hook (org-mode . org-bullets-mode)
   :custom
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
@@ -300,10 +288,10 @@
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
+  :straight t
   :hook (org-mode . efs/org-mode-visual-fill))
 
 (defun efs/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
   (lsp-headerline-breadcrumb-mode))
 
 (use-package lsp-mode
@@ -314,11 +302,12 @@
   :config
   (lsp-enable-which-key-integration t))
 
-(use-package python-mode
-  :ensure t
-  :hook (python-mode . lsp-deferred))
+;; (use-package python-mode
+;;   :ensure t
+;;   :hook (python-mode . lsp-deferred))
 
 (use-package lsp-ui
+  :straight t
   :hook (lsp-mode . lsp-ui-mode)
   :custom
   (lsp-ui-doc-position 'bottom))
@@ -326,6 +315,7 @@
 (use-package ement)
 
 (use-package blacken
+  :straight t
   :hook ((python-mode . blacken-mode)))
 
 (use-package yasnippet-snippets)
@@ -334,10 +324,9 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-(use-package evil-tex)
-
-(use-package iedit)
-(use-package flymake-cursor)
+(straight-use-package 'evil-tex)
+(straight-use-package 'iedit)
+(straight-use-package 'flymake-cursor)
 
 (use-package cedet)
 (semantic-mode 1)
@@ -357,12 +346,22 @@
 (all-the-icons-completion-mode)
 (setq c-default-style "bsd"
       c-basic-offset 8)
-(provide 'init.el)
-;;; init.el ends here
 
 ;; Got
 (use-package vc-got
+  :straight t
   :config
   (setq vc-got-program "~/bin/got"))
 
+(setq debug-on-error t)
+
+
+(defun force-debug (func &rest args)
+  (condition-case e
+      (apply func args)
+    ((debug error) (signal (car e) (cdr e)))))
+
+(advice-add #'vertico--exhibit :around #'force-debug)
+(provide 'init.el)
+;;; init.el ends here
 
